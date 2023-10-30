f00500628c6af59b23391a912eeca19e17d91574
MatheoDev <matheo.deknuydt@gmail.com>
2023-10-30 16:01:08 +0100

    auth firebase

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import { store } from './src/helpers/store'
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { useAppDispatch } from './src/helpers/hook'
+import { createUser, loginUser, logoutUser } from './src/helpers/userSlice'
 
-export default function App() {
+const user = {
+  email: 'matheo.deknuydt@gmail.com',
+  password: 'Test1234@',
+} as UserType
+
+const WrapperApp = () => {
+  const dispatch = useAppDispatch()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity className='my-4' onPress={() => dispatch(loginUser(user))}>
+        <Text>Connect</Text>
+      </TouchableOpacity>
+
+
+      <TouchableOpacity className='my-4' onPress={() => dispatch(logoutUser())}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity className='my-4' onPress={() => dispatch(createUser(user))}>
+        <Text>Sign up</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  export default function App() {
+    return (
+      <Provider store={store}>
+        <WrapperApp />
+      </Provider>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
