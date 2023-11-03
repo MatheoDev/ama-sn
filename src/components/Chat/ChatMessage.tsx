eaf7550f69bfd8d7b6a3e7fb818199ca2d398c81
MatheoDev <matheo.deknuydt@gmail.com>
2023-11-03 15:15:34 +0100

    bugfix

--- src/components/Chat/ChatMessage.tsx
+++ src/components/Chat/ChatMessage.tsx
@@ -11,7 +11,7 @@ const ChatMessage = ({chat}: ChatMessageProp) => {
   const user = useAppSelector(selectUserConnected)
   const isSender = chat.from === user?.uid
   const timestamp = new Date(chat.date.seconds * 1000)
-  const date = `${timestamp.getDate()}/${timestamp.getMonth() + 1}/${timestamp.getFullYear()} ${timestamp.getHours()}:${timestamp.getMinutes()}`
+  const date = `${timestamp.getDate()}/${timestamp.getMonth() + 1}/${timestamp.getFullYear()} ${timestamp.getHours() < 10 ? '0' + timestamp.getHours() : timestamp.getHours()}:${timestamp.getMinutes() < 10 ? '0' + timestamp.getMinutes() : timestamp.getMinutes()}`
 
   return (
     <View className={`${isSender ? 'bg-blue-500' : 'bg-gray-300'} rounded-md my-2 mx-2 p-2`}>
